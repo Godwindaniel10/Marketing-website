77f294c1ea91107b98438e81813a115466bb3862
godwin olele <godwinolele10@gmail.com>
2021-08-23 08:35:40 +0100

    contact section half done

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -6,7 +6,10 @@ const contact = () => {
         <>
            <div className="contact-container">
               <div className="header-container">
-                  <div className="box1"></div>
+                  <div className="box1">
+                      <h1>Get In Touch</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex atque dicta minima inventore nesciunt, fuga nemo. Magni, dolorum voluptates. Voluptate porro voluptatibus commodi nobis!</p>
+                  </div>
                   <div className="contact-form"></div>
               </div>
               <div className="map-section"></div>
